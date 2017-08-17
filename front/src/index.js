4e25d77e754b9eb4da231e19a54d8b368b287421
anthony <anthony.sarais@greencom-networks.com>
2017-08-17 11:38:48 +0200

    removed node_modules dependencies in index.html

--- front/src/index.js
+++ front/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import {AppContainer} from 'react-hot-loader'
 import Router from './Router';
 
+import './libs/css/materialIcons.css';
+import 'materialize-css/dist/css/materialize.min.css';
+import 'materialize-css/dist/js/materialize.min';
 import './core.scss';
 
 const render = Component => {
--- front/webpack.config.js
+++ front/webpack.config.js
@@ -3,10 +3,15 @@ const path = require('path');
 
 module.exports = {
     plugins: [
+        new webpack.ProvidePlugin({
+            'jQuery': 'jquery',
+            'window.jQuery': 'jquery',
+            '$': 'jquery'
+        }),
         new webpack.DefinePlugin({
             'NODE_ENV': JSON.stringify(process.env.NODE_ENV),
             'GH_PAGE': JSON.stringify(process.env.GH_PAGE) || false,
-            'SERVER_URL': JSON.stringify(process.env.SERVER_URL || 'http://localhost:3000')
+            'SERVER_URL': JSON.stringify(process.env.SERVER_URL || 'http://localhost:3000'),
         })
     ],
     devtool: 'eval',
